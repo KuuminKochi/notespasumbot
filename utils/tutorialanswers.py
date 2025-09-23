91e082162f31180991469b1013475c5bbf249d48
KuuminKochi <yesimkurisu@gmail.com>
2025-09-24 01:48:38 +0800

    lecturee notes and tutos

---
+++ utils/tutorialanswers.py
@@ -0,0 +1,14 @@
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+from dotenv import load_dotenv
+from utils import globals
+import os
+
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")
+NOTES_PASUM = int(os.getenv("NOTES_PASUM"))
+ADMIN_NOTES = int(os.getenv("ADMIN_NOTES"))
+
+async def tutorial_answers(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(globals.TUTORIAL_ANSWERS)
